34b6117e4ebd959c62b016ce0b02dcd2ecb58354
Gayatri Pisya <gayatripisya@gmail.com>
2025-03-07 12:05:36 +0530

    updated

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -3,7 +3,7 @@ session_start();
 include 'db.php';
 
 define("ADMIN_EMAIL", "gayatripisya@gmail.com");  // Change to your admin email
-define("ADMIN_HASHED_PASSWORD", "26265fd51f8850901201ef1e79dce080"); // MD5 of your password
+define("ADMIN_HASHED_PASSWORD", "52c76da7c56b606849df5a038d1bb561"); // MD5 of your password
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = trim($_POST['email']);
