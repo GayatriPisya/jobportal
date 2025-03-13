1d74e902a5f967e932f2a33fc568876b564a6e31
Gayatri Pisya <gayatripisya@gmail.com>
2025-03-13 22:48:55 +0530

    updated

--- jobseeker/jobseeker.php
+++ jobseeker/jobseeker.php
@@ -14,12 +14,12 @@ if (isset($_POST['keyword']) && !empty(trim($_POST['keyword']))) {
         while ($row = $result->fetch_assoc()) {
             echo "<div class='card mb-3'>
                     <div class='card-body'>
-                        <h5 class='card-title'>{$row['title']}</h5>
-                        <p class='card-text'>{$row['description']}</p>
-                        <p><strong>Category:</strong> {$row['category']}</p>
-                        <p><strong>Location:</strong> {$row['location']}</p>
-                        <p><strong>Salary:</strong> \${$row['salary']}</p>
-                        <a href='{$row['apply_link']}' target='_blank' class='btn btn-success'>Apply Now</a>
+                        <h5 class='card-title'><i class='fas fa-briefcase mr-1'></i>&nbsp;{$row['title']}</h5>
+                        <p class='card-text'><i class='fas fa-file-alt mr-1'></i>&nbsp;{$row['description']}</p>
+                        <p><i class='fas fa-tags mr-1'></i>&nbsp;<strong>Category:</strong> {$row['category']}</p>
+                        <p><i class='fas fa-map-marker-alt mr-1'></i>&nbsp;<strong>Location:</strong> {$row['location']}</p>
+                        <p><i class='fas fa-dollar-sign mr-1'></i>&nbsp;<strong>Salary:</strong> \${$row['salary']}</p>
+                        <a href='{$row['apply_link']}' target='_blank' class='btn btn-success'><i class='fas fa-external-link-alt mr-1'></i>&nbsp;Apply Now</a>
                     </div>
                   </div>";
         }
@@ -33,3 +33,4 @@ if (isset($_POST['keyword']) && !empty(trim($_POST['keyword']))) {
     echo "<p>Please enter a valid keyword to search.</p>";
 }
 ?>
+
